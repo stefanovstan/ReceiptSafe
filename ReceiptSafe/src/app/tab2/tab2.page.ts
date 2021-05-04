a7026e737c0c1202ddfec6241d97f11018002ac2
BrettLogeais <blogeais@email.sc.edu>
2021-05-03 20:45:21 -0400

    fixed pin bug

--- ReceiptSafe/src/app/tab2/tab2.page.ts
+++ ReceiptSafe/src/app/tab2/tab2.page.ts
@@ -98,10 +98,11 @@ export class Tab2Page implements OnInit {
 
   scan() {
     const options: CameraOptions = {
-      quality: 100,
+      quality: 25,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false
     }
     var page=this;
     this.camera.getPicture(options).then((imageData) => {
